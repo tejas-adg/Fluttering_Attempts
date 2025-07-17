b8493cea09b4b9329f393a310993dbccd92ca064
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2025-07-17 16:43:10 -0400

    Update README and main.dart to enhance UI and add favorites functionality using a navigation rail

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This is the branch where I created my first flutter app all on my own, by follow
 The `pubspec.yaml` file specifies basic information about your app, such as its current version, its dependencies, and the assets with which it will ship.
 
 Next, open another configuration file in the project, `analysis_options.yaml`. This file determines how strict Flutter should be when analyzing your code. Since this is your first foray into Flutter, you're telling the analyzer to take it easy. You can always tune this later. In fact, as you get closer to publishing an actual production app, you will almost certainly want to make the analyzer stricter than this.
+
+Note: Flutter works with logical pixels as a unit of length. They are also sometimes called device-independent pixels. A padding of 8 pixels is visually the same regardless of whether the app is running on an old low-res phone or a newer ‘retina' device. There are roughly 38 logical pixels per centimeter, or about 96 logical pixels per inch, of the physical display.
--- lib/main.dart
+++ lib/main.dart
@@ -76,6 +76,7 @@ class MyAppState extends ChangeNotifier {
     notifyListeners(); // Notifies all widgets that are listening to this state (a method of ChangeNotifier)
     // ensures that anyone watching MyAppState is notified.
   }
+
   /* Added a new property to MyAppState called favorites.
   This property is initialized with an empty set: {}.
 
@@ -106,13 +107,112 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+/* The underscore (_) at the start of _MyHomePageState makes
+the class private and is enforced by the compiler.
+If you want to know more about privacy in Dart,
+and other topics, read the Language Tour. https://dart.dev/language/libraries*/
+class _MyHomePageState extends State<MyHomePage> {
+  var currentIndex = 0;
 
   /* Every widget defines a build() method
   that's automatically called every time the widget's circumstances change
   so that the widget is always up to date. */
   @override
   Widget build(BuildContext context) {
+
+    Widget page;
+    switch (currentIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $currentIndex');
+    }
+
+    /* Every build method must return a widget
+    or (more typically) a nested tree of widgets.
+    In this case, the top-level widget is Scaffold.
+    You aren't going to work with Scaffold in this guide,
+    but it's a helpful widget and is found in the vast majority of real-world Flutter apps.*/
+
+    /*
+    LayoutBuilder's builder callback is called every time the constraints change.
+    
+    This happens when, for example:
+      - The user resizes the app's window
+      - The user rotates their phone from portrait mode to landscape mode, or back
+      - Some widget next to MyHomePage grows in size, making MyHomePage's constraints smaller
+    */
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              /* The SafeArea ensures that its child is
+              not obscured by a hardware notch or a status bar.
+              In this app, the widget wraps around NavigationRail
+              to prevent the navigation buttons from being obscured
+              by a mobile status bar, for example.*/
+              SafeArea(
+                child: NavigationRail(
+                  /*change the extended: `false` 
+                  line in NavigationRail to `true`.
+                  This shows the labels next to the icons.
+                  In a future step, you will learn how to
+                  do this automatically when the app has
+                  enough horizontal space.*/
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  /* A selected index of zero selects the first destination,
+                  a selected index of one selects the second destination, and so on...
+                  For now, it's hard coded to zero.*/
+                  selectedIndex: currentIndex,
+                  onDestinationSelected: (value) {
+                    /*is similar to the notifyListeners() method used previously
+                    it makes sure that the UI updates.*/
+                    setState(() {
+                      currentIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryFixed,
+                  child: page, // current page based on the selected index
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
     /* MyHomePage tracks changes to the app's current state using the watch method.*/
     var appState = context.watch<MyAppState>();
     var currentPair = appState.current;
@@ -121,54 +221,48 @@ class MyHomePage extends StatelessWidget {
         ? Icons.favorite
         : Icons.favorite_border;
 
-    /* Every build method must return a widget
-    or (more typically) a nested tree of widgets.
-    In this case, the top-level widget is Scaffold.
-    You aren't going to work with Scaffold in this guide,
-    but it's a helpful widget and is found in the vast majority of real-world Flutter apps.*/
-    return Scaffold(
+    return Center(
+      
       /* Column is one of the most basic layout widgets in Flutter.
       It takes any number of children and puts them in a column from top to bottom.
       By default, the column visually places its children at the top.
       You'll soon change this so that the column is centered.*/
-      body: Center(
-        child: Column(
-          // Centers the column's children vertically
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PairDisplayCard(
-              displayedPair: currentPair,
-            ), // The Text widget no longer refers to the whole appState.
-            SizedBox(height: 10),
-            Row(
-              // mainAxisSize.min is used so the row only takes up as much horizontal space as its children need.
-              mainAxisSize: MainAxisSize.min, // Takes up space only as needed
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Calls the toggleFavorite method in MyAppState
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(favoriteIcon),
-                  label: Text("Favorite"),
-                ),
-                SizedBox(width: 10), // Adds space between the buttons
-                ElevatedButton(
-                  onPressed: () {
-                    // print("button pressed"); // This prints to the console
-                    // Calls the getNext method in MyAppState to get a new word pair
-                    appState.getNext();
-                  },
-                  child: Text("next"),
-                ),
-              ],
-            ),
-          ],
-        ),
+      child: Column(
+        // Centers the column's children vertically
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          PairDisplayCard(displayedPair: currentPair),
+          SizedBox(height: 10),
+          Row(
+            // mainAxisSize.min is used so the row only takes up as much horizontal space as its children need.
+            mainAxisSize: MainAxisSize.min, // Takes up space only as needed
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Calls the toggleFavorite method in MyAppState
+                  appState.toggleFavorite();
+                },
+                icon: Icon(favoriteIcon),
+                label: Text('favorite'),
+              ),
+              SizedBox(width: 10), // Adds space between the buttons
+              ElevatedButton(
+                onPressed: () {
+                  // print("button pressed"); // This prints to the console
+                  // Calls the getNext method in MyAppState to get a new word pair
+                  appState.getNext();
+                },
+                child: Text('next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// ...
 
 class PairDisplayCard extends StatelessWidget {
   const PairDisplayCard({super.key, required this.displayedPair});
@@ -180,14 +274,14 @@ class PairDisplayCard extends StatelessWidget {
     final theme = Theme.of(context);
 
     return Card(
-      color: theme.colorScheme.primaryFixed,
+      color: theme.colorScheme.primary,
       elevation: 3,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(
           displayedPair.asLowerCase,
           style: theme.textTheme.displayMedium!.copyWith(
-            color: theme.colorScheme.onPrimaryFixed,
+            color: theme.colorScheme.onPrimary,
           ),
           semanticsLabel: "${displayedPair.first} ${displayedPair.second}",
         ),
