0578407b6de21106e8a129b84650fca4b0b7df68
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2025-07-16 20:31:29 -0400

    Enhance MyHomePage UI with a button and update text for clarity

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,17 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+        children: [
+          Text('A random AWESOME idea:'),
+          Text(appState.current.asLowerCase),
+
+          ElevatedButton(
+            onPressed: () {
+              print("button pressed");
+            },
+            child: Text("next"),
+          ),
+        ],
       ),
     );
   }
